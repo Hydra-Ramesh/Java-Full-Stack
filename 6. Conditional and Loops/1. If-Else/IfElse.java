4e960af8a536337d66d107910e1b9df0b30236a1
Ramesh Das <rameshdas@Rameshs-MacBook-Air.local>
2025-03-15 11:45:31 +0530

    Add the Loops

---
+++ 6. Conditional and Loops/1. If-Else/IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if (num > 0) {
+            System.out.println("The number is positive.");
+        }
+
+        System.out.println("This statement is always executed.");
+    }
+}
